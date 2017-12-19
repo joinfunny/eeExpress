8f50c35a20d045eb5927f2d24f3d0a3726910041
jiangfeng <jiangfeng@rongcapital.cn>
2017-12-19 11:16:09 +0800

    update lib

--- test/index.js
+++ test/index.js
@@ -3,7 +3,12 @@ require('should')
 let eeExpress = require('../lib/index')
 let config = {
   runtime: {
-    disableOrmMapping: false,
+    'disableAuthorize': true,
+    'disableOrmMapping': true,
+    'disableSession': true,
+    'disableLog': true,
+    'disableInternational': true,
+    'disableGzip': true,
     'directory': {
       'static': './test/app/statics',
       'view': './test/app/views',
@@ -49,35 +54,3 @@ let config = {
 eeExpress.config(config)
 
 eeExpress.run()
-
-function check() {
-  var complaint = {
-    'docmentsNo': '2135535',
-    'agentOrderNo': '5200012741201709290991109507',
-    'feedback': '充错号码',
-    'phoneNo': '15975807466',
-    'coustomerRequest': '退款',
-    'complaintSources': '客服',
-    'timeLength': '0小时',
-    'times': '1',
-    'satisfaction': '0星',
-    'record': '用户投诉; 客服工号3161624341认领了工单;',
-    'orderTime': '2017-09-30 00:01:03',
-    'type': 1
-  }
-
-  eeExpress.runtime.OrmMapping.models.complaints.create(complaint)
-    .then(function () {
-      return eeExpress.runtime.OrmMapping.models.complaints.destroy({
-        'docmentsNo': '2135535'
-      }).then(function (items) {
-        if (items.length === 1) {
-          eeExpress.logger.info('测试Mongodb操作成功')
-        } else {
-          eeExpress.logger.info('测试Mongodb操作失败')
-        }
-      })
-    })
-}
-
-check()
