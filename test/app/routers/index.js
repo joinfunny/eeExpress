a9cc35cfa73e7b5db82b90ee1d1946700d0ce4e6
jiangfeng <jiangfeng@rongcapital.cn>
2017-12-19 11:37:25 +0800

    update

--- test/app/routers/index.js
+++ test/app/routers/index.js
@@ -1,10 +1,13 @@
 let eeExpress = require('../../../lib/index')
 var router = eeExpress.Router()
 
+let index = 0
 
-router.get('/', function (req, res, next) {
-  eeExpress.logger.info('test view index')
-  res.render('index')
+router.get('/', function(req, res, next) {
+  index++
+  eeExpress.logger.info('test view index :' + index)
+  res.send('ok')
+  //res.render('index')
 })
 
 
