a0df1d2f96778c3f4718aa9b7378adf547c5e8ca
jiangfeng <jiangfeng@rongcapital.cn>
2017-12-20 17:59:40 +0800

    add cluster

---
+++ cluster.js
@@ -0,0 +1,22 @@
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+    console.log("master start...");
+
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('listening',function(worker,address){
+        console.log('listening: worker ' + worker.process.pid +', Address: '+address.address+":"+address.port);
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    require('./test/index')
+}
