0f2e629830d415b4de9e8544770d706a127b3e29
jiangfeng <jiangfeng@rongcapital.cn>
2017-12-20 18:06:31 +0800

    update

--- test/app/routers/index.js
+++ test/app/routers/index.js
@@ -1,13 +1,14 @@
 let eeExpress = require('../../../lib/index')
 var router = eeExpress.Router()
-
+var cluster = require('cluster');
 let index = 0
 
 router.get('/', function(req, res, next) {
   index++
   //eeExpress.logger.info('test view index :' + index)
-  res.end('ok')
+  //res.end('ok')
   //res.render('index')
+  res.end('worker'+cluster.worker.id+',PID:'+process.pid);
 })
 
 
