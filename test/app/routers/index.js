4e9878ece626acc6a28f5d76393c53583bf654c0
jiangfeng <jiangfeng@rongcapital.cn>
2017-12-25 09:26:49 +0800

    update

--- test/app/routers/index.js
+++ test/app/routers/index.js
@@ -1,12 +1,16 @@
 let eeExpress = require('../../../lib/index')
 var router = eeExpress.Router()
-var cluster = require('cluster');
 
 
 router.get('/', function(req, res, next) {
   let count = req.query.count || 0
+  let objCount = req.query.objCount || 0
   for(var i = 0; i < count; i++) {
     console.log(i)
+  }
+  let obj = {}
+  for(var o = 0; o< objCount ; o++) {
+    obj[objCount] = objCount
   }
   res.end(req.sessionID)
 })
