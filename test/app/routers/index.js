e8015e2cb7050999225cd7bb2e0df3db256aaff9
jiangfeng <jiangfeng@rongcapital.cn>
2018-01-17 12:33:19 +0800

    add logs route

--- test/app/routers/index.js
+++ test/app/routers/index.js
@@ -1,6 +1,7 @@
 let eeExpress = require('../../../lib/index')
 var router = eeExpress.Router()
-
+const fs = require('fs')
+const path = require('path')
 
 router.get('/', function(req, res, next) {
   let count = req.query.count || 0
@@ -13,6 +14,27 @@ router.get('/', function(req, res, next) {
     obj[objCount] = objCount
   }
   res.end(req.sessionID)
+})
+
+router.get('/logs',function(req,res,next){
+  let basePath = path.resolve('./test/app/statics/logs/');
+  console.log(basePath);
+  fs.readdir('./test/app/statics/logs', function(err, files) {
+      var list = [];
+      files.forEach(function(file, index) {
+          let filePath = path.resolve('./test/app/statics/logs', file);
+          let extName = path.extname(filePath)
+          let statu = fs.statSync(filePath);
+          if (statu.isFile() && extName === '.log' ) {
+              list.push('<li><a href="/logs/' + file + '">' + file + '</a></li>');
+          }
+      });
+      res.status(200).type('html').send('\
+                  <h1>日志</h1>\
+                  <hr>\
+                  <ul>' + list.join('') + '</ul>')
+          .end();
+  });
 })
 
 module.exports = router
