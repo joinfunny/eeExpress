fe7744e18509e1788c2e4d65765c905599bd5989
joinfunny <jiangfeng_130122@163.com>
2018-02-26 20:30:51 +0800

    update service register

--- test/app/services/login.js
+++ test/app/services/login.js
@@ -1,21 +1,25 @@
 
-module.exports = {
-  '/api/test': {
-    method: 'get',
-    mock: {
-      user: 1
-    },
-    beforeRequest: function(req, res) {
-      req.query.id = 1
-    },
-    request: function(req, res, callback) {
-      callback({
-        success: true,
-        msg: req.sessionID
-      })
-    },
-    afterRequest: function(req, res, responseData) {
-      responseData.data = 'test'
+module.exports = app => {
+  return {
+    '/api/test': {
+      method: 'get',
+      mock: {
+        user: 1
+      },
+      beforeRequest: function(req, res) {
+        req.query.id = 1
+      },
+      request: function(req, res, callback) {
+        callback({
+          success: true,
+          msg: req.sessionID
+        })
+      },
+      afterRequest: function(req, res, responseData) {
+        responseData.data = {
+          config: app.EE_EXPRESS_CONFIG
+        }
+      }
     }
   }
 }
